35e70be530fa2b04550487c44903dcd35b713159
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-04 15:05:41 -0300

    feat(dto): add active field to TransactionResponseDTO

--- backend/dto/transaction/TransactionResponseDTO.py
+++ backend/dto/transaction/TransactionResponseDTO.py
@@ -9,6 +9,7 @@ class TransactionResponseDTO(BaseModel):
     transaction_date: date
     type: TypeEnum
     amount: float
+    active: bool
     category_id: int
     user_id: int
     created_at: datetime
