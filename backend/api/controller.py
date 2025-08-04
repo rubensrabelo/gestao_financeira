3b1124682389c3ad4d0236c250c282201a4e39ef
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-04 10:21:54 -0300

    chore(transaction): add transactionRouter to api controller imports and router registration

--- backend/api/controller.py
+++ backend/api/controller.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from .routes import user_router, auth_router, category_router
+from .routes import (
+    user_router, auth_router, category_router, transaction_router
+)
 
 api_router = APIRouter()
 
@@ -9,7 +11,9 @@ api_router.include_router(user_router, prefix="/users", tags=["User"])
 api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
 api_router.include_router(
-    category_router,
-    prefix="/categories",
-    tags=["Category"]
+    category_router, prefix="/categories", tags=["Category"]
+)
+
+api_router.include_router(
+    transaction_router, prefix="/transactions", tags=["Transaction"]
 )
--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -1,9 +1,11 @@
 from .userRouter import router as user_router
 from .authRouter import router as auth_router
 from .categoryRouter import router as category_router
+from .transactionRouter import router as transaction_router
 
 __all__ = [
     "user_router",
     "auth_router",
-    "category_router"
+    "category_router",
+    "transaction_router"
 ]
