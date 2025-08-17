1e53f2724de5e30b5e0bb35eee7f1b276b5d8631
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-17 15:15:49 -0300

    fix(transaction): fix DB connection and support transaction edit

--- backend/database.py
+++ backend/database.py
@@ -15,5 +15,6 @@ def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
 
 
-def get_session() -> Session:
-    return Session(engine)
+def get_session():
+    with Session(engine) as session:
+        yield session
